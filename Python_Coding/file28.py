e2e35c1f7cd7897733ef82e606380c12b07f9b29
Sheri_1434 <itssheri1434@gmail.com>
2024-09-01 12:35:18 +0500

    stack implementation

--- Python_Coding/file28.py
+++ Python_Coding/file28.py
@@ -1,3 +1,50 @@
 # Python Stack Implementation:
 
- 
+#Queue: First Item CAlls at first.
+#Stack: First item Calls at last
+
+# stack = []
+
+# while True:
+#     var = input("Enter 1 to continue:")
+#     if var == "0":
+#         break
+
+#     if var == "1":
+#         stack1 = input("Enter names to append:")
+#         stack.append(stack1)
+
+#     elif var == "2":
+#         if len (stack) <= 0:
+#             print("No name left to pop")
+#         else:
+#             x = stack.pop()
+#             print(f"Popped name is {x}")
+#     else:
+#         print("Invalid Response")
+#     print(stack)
+
+
+
+# Queue:
+
+queue = []
+
+while True:
+    var = input("Enter 1 to continue:")
+    if var == "0":
+        break
+
+    if var == "1":
+        stack1 = input("Enter names to append:")
+        queue.append(stack1)
+
+    elif var == "2":
+        if len (queue) <= 0:
+            print("No name left to pop")
+        else:
+            x = queue.pop(0)
+            print(f"Popped name is {x}")
+    else:
+        print("Invalid Response")
+    print(queue)
